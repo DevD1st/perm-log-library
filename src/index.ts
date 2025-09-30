8081f6a0c406b480f5faf0925744fc2d681d29eb
DevD1st <olanrewajuyusuf379@gmail.com>
2025-09-30 13:18:04 +0100

    easy import and .d.ts files creation
    - import and export all files from index.ts so users of libraty can directly impot these files from index.js (instead of /error /util, ...) if they want to
    - modify ts config to emit type files (.d.ts)

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import * as publisher from "./publisher";
+import * as listener from "./listener";
+import * as util from "./util";
+import * as error from "./error";
+
+export { publisher, listener, util, error };
